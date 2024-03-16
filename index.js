dec92f495a1398b65d08f92a2d565e29185dbb0a
aspmohan15 <aspmohan15@gmail.com>
2024-03-16 12:59:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let value = new Array(10000).fill("Mohan")
+
+const loopFunction = (arr) => {
+  let t1 = performance.now()
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+  let t2 = performance.now()
+  console.log(t2-t1);
+};
+
+loopFunction(value)
+
